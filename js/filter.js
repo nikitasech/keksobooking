8937f846b1e3483608e4743458dcc30ee79bb805
NikitaSech <NikitaSech@yandex.ru>
2020-07-22 14:57:09 +0300

    Реализована фильтрация

--- js/filter.js
+++ js/filter.js
@@ -18,95 +18,15 @@
     HIGH_MIN: 50000
   };
 
-  function filterType(data) {
+  function checkNeedFilter(data, element, callback) {
     var filterData;
 
-    /* Если выбираем "любой тип жилья" - в переменную отфильтрованных
+    /* Если выбираем пункт не требующий фильтрации - в переменную отфильтрованных
     объявлений записываеются все объявления. Иначе фильтруются по типу */
-    if (housingTypeElement.value === 'any') {
+    if (element.value === 'any' || !element.length) {
       filterData = data;
     } else {
-      filterData = data
-        .filter(function (ad) {
-          return ad.offer.type === housingTypeElement.value;
-        });
-    }
-    return filterData;
-  }
-
-  function filterPrice(data) {
-    var filterData;
-
-    if (priceElement.value === 'any') {
-      filterData = data;
-    } else {
-      filterData = data
-        .filter(function (ad) {
-          if (priceElement.value === 'low') {
-            return ad.offer.price < Price.LOW_MAX;
-          } else if (priceElement.value === 'middle') {
-            return ad.offer.price >= Price.MIDDLE_MIN && ad.offer.price < Price.MIDDLE_MAX;
-          }
-          return ad.offer.price >= Price.HIGH_MIN;
-        });
-    }
-    return filterData;
-  }
-
-  function filterRooms(data) {
-    var filterData;
-
-    if (roomsElement.value === 'any') {
-      filterData = data;
-    } else {
-      filterData = data
-        .filter(function (ad) {
-          return +ad.offer.rooms === +roomsElement.value;
-        });
-    }
-    return filterData;
-  }
-
-  function filterGuests(data) {
-    var filterData;
-
-    if (guestsElement.value === 'any') {
-      filterData = data;
-    } else {
-      filterData = data
-        .filter(function (ad) {
-          return +ad.offer.guests === +guestsElement.value;
-        });
-    }
-    return filterData;
-  }
-
-  function filterFeatures(data) {
-    var filterData;
-    var checkedFeatures = getCheckedFeatures();
-
-    if (checkedFeatures.length === 0) {
-      filterData = data;
-    } else {
-      filterData = data
-        .filter(function (ad) {
-          var isCoincidence;
-
-          for (var i = 0; i < checkedFeatures.length; i++) {
-            for (var j = 0; j < ad.offer.features.length; j++) {
-              if (checkedFeatures[i] === ad.offer.features[j]) {
-                isCoincidence = true;
-                break;
-              } else {
-                isCoincidence = false;
-              }
-            }
-            if (!isCoincidence) {
-              return false;
-            }
-          }
-          return true;
-        });
+      filterData = callback(data);
     }
     return filterData;
   }
@@ -123,19 +43,69 @@
     return checkedFeatures;
   }
 
+  function filterType(data) {
+    return data.filter(function (ad) {
+      return ad.offer.type === housingTypeElement.value;
+    });
+  }
+
+  function filterPrice(data) {
+    return data.filter(function (ad) {
+      if (priceElement.value === 'low') {
+        return ad.offer.price < Price.LOW_MAX;
+      } else if (priceElement.value === 'middle') {
+        return ad.offer.price >= Price.MIDDLE_MIN && ad.offer.price < Price.MIDDLE_MAX;
+      }
+      return ad.offer.price >= Price.HIGH_MIN;
+    });
+  }
+
+  function filterRooms(data) {
+    return data.filter(function (ad) {
+      return +ad.offer.rooms === +roomsElement.value;
+    });
+  }
+
+  function filterGuests(data) {
+    return data.filter(function (ad) {
+      return +ad.offer.guests === +guestsElement.value;
+    });
+  }
+
+  function filterFeatures(data) {
+    return data.filter(function (ad) {
+      var checkedFeatures = getCheckedFeatures();
+      var isCoincidence;
+
+      for (var i = 0; i < checkedFeatures.length; i++) {
+        for (var j = 0; j < ad.offer.features.length; j++) {
+          if (checkedFeatures[i] === ad.offer.features[j]) {
+            isCoincidence = true;
+            break;
+          } else {
+            isCoincidence = false;
+          }
+        }
+        if (!isCoincidence) {
+          return false;
+        }
+      }
+      return true;
+    });
+  }
+
   function filterToggle() {
     window.Util.toggleInputs(adsFilterElements); // Разблокируем поля фильтров
   }
 
   function renderFilterAds(data) {
-    var filterData = filterType(data);
-    filterData = filterPrice(filterData);
-    filterData = filterRooms(filterData);
-    filterData = filterGuests(filterData);
-    filterData = filterFeatures(filterData);
+    var checkedFeatures = getCheckedFeatures();
 
-    getCheckedFeatures();
-
+    var filterData = checkNeedFilter(data, housingTypeElement, filterType);
+    filterData = checkNeedFilter(filterData, priceElement, filterPrice);
+    filterData = checkNeedFilter(filterData, roomsElement, filterRooms);
+    filterData = checkNeedFilter(filterData, guestsElement, filterGuests);
+    filterData = checkNeedFilter(filterData, checkedFeatures, filterFeatures);
 
     window.map.renderPins(filterData.slice(0, MAX_NUMBER_ADS));
   }
